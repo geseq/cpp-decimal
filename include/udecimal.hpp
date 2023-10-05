bbe87f3d18459075d07b54d10c4c31e91a38890c
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-10-05 14:23:33 +0100

    more improvements (#12)
    * use precomputed power of 10
    
    * minor improvements
    
    * consistent styling
    
    * improve to_string
    
    * use precomputed power in round

--- include/udecimal.hpp
+++ include/udecimal.hpp
@@ -11,6 +11,8 @@
 #include <string>
 #include <vector>
 
+namespace udecimal {
+
 #if defined(__GNUC__) || defined(__clang__)
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
@@ -18,8 +20,6 @@
 #define likely(x) (x)
 #define unlikely(x) (x)
 #endif
-
-namespace udecimal {
 
 template <typename T, typename = void>
 struct has_int128_impl : std::false_type {};
@@ -38,6 +38,35 @@ constexpr uint64_t const_pow() {
     return result;
 }
 
+uint64_t precomputed_pow_10(unsigned int exponent) {
+    constexpr std::array<uint64_t, 20> powers_of_10 = {1,
+                                                       10,
+                                                       100,
+                                                       1000,
+                                                       10000,
+                                                       100000,
+                                                       1000000,
+                                                       10000000,
+                                                       100000000,
+                                                       1000000000,
+                                                       10000000000,
+                                                       100000000000,
+                                                       1000000000000,
+                                                       10000000000000,
+                                                       100000000000000,
+                                                       1000000000000000,
+                                                       10000000000000000,
+                                                       100000000000000000,
+                                                       1000000000000000000,
+                                                       10000000000000000000UL};
+
+    if (unlikely(exponent >= powers_of_10.size())) {
+        throw std::invalid_argument("invalid exponent");
+    }
+
+    return powers_of_10[exponent];
+}
+
 // Decimal is a decimal precision unsigned number (defaults to 11.8 digits).
 // IMPORTANT: this is not designed to be used for signed decimals.
 template <int nPlaces = 8>
@@ -54,19 +83,22 @@ class Decimal {
     static_assert(nPlaces < digits);
     static_assert(nPlaces > 0);
 
-    static std::string zeros() { return {std::string(nPlaces, '0')}; }
-    static std::runtime_error errTooLarge;
-    static std::runtime_error errFormat;
+    inline static std::string zeros() { return {std::string(nPlaces, '0')}; }
+
+    static const std::runtime_error errDivByZero;
+    static const std::overflow_error errTooLarge;
+    static const std::overflow_error errOverflow;
+    static const std::invalid_argument errInvalidInput;
 
     Decimal(const Decimal<nPlaces>& other) : fp(other.fp) {}
 
     // Creates a Decimal from a double, rounding at the 8th decimal place
     Decimal(double f) {
         if (unlikely(std::isnan(f))) {
-            throw std::invalid_argument("invalid input");
+            throw errInvalidInput;
         }
         if (unlikely(f >= MAX || f < 0)) {
-            throw std::invalid_argument("invalid input");
+            throw errInvalidInput;
         }
         double round = 0.5;
         if (f < 0) {
@@ -91,14 +123,14 @@ class Decimal {
         if (period == std::string::npos) {
             i = std::stoull(s);
             if (unlikely(i > MAX)) {
-                throw std::overflow_error("number too large");
+                throw errTooLarge;
             }
             fp = i * scale;
         } else {
             if (period > 0) {
                 i = std::stoull(s.substr(0, period));
                 if (unlikely(i > MAX)) {
-                    throw std::overflow_error("number too large");
+                    throw errTooLarge;
                 }
             }
             std::string fs = s.substr(period + 1);
@@ -109,24 +141,24 @@ class Decimal {
     }
 
     // New returns a new fixed-point decimal, value * 10 ^ exp.
-    static Decimal FromExp(uint64_t value, int32_t exp) {
+    static Decimal FromExp(uint64_t value, int exp) {
         if (exp >= 0) {
-            uint64_t exp_mul = std::pow(10, exp);
+            uint64_t exp_mul = precomputed_pow_10(exp);
             return {mul(newI(value, 0), newI(exp_mul, 0))};
         }
 
-        return {newI(value, static_cast<uint32_t>(-exp))};
+        return {newI(value, static_cast<unsigned int>(-exp))};
     }
 
-    [[nodiscard]] bool IsZero() const { return fp == 0; }
+    [[nodiscard]] bool is_zero() const { return fp == 0; }
 
     // Float converts the Decimal to a float64
-    [[nodiscard]] double Double() const { return static_cast<double>(fp) / scale; }
+    [[nodiscard]] double to_double() const { return static_cast<double>(fp) / scale; }
 
     // Add adds f0 to f producing a Decimal.
     Decimal operator+(const Decimal& f0) const {
         if (unlikely(f0.fp > UINT64_MAX - fp)) {
-            throw std::overflow_error("decimal overflow");
+            throw errOverflow;
         }
         return Decimal{fp + f0.fp};
     }
@@ -134,7 +166,7 @@ class Decimal {
     // Sub subtracts f0 from f producing a Decimal.
     Decimal operator-(const Decimal& f0) const {
         if (unlikely(fp < f0.fp)) {
-            throw std::overflow_error("decimal overflow");
+            throw errOverflow;
         }
         return Decimal{fp - f0.fp};
     }
@@ -144,7 +176,7 @@ class Decimal {
     Decimal operator/(const Decimal& f0) const {
         if constexpr (has_int128) {
             if (f0.fp == 0) {
-                throw std::runtime_error("Division by zero.");
+                throw errDivByZero;
             }
 
             __int128 temp = static_cast<__int128>(fp) * scale;
@@ -152,7 +184,7 @@ class Decimal {
             temp /= f0.fp;
 
             if (temp > std::numeric_limits<uint64_t>::max()) {
-                throw std::overflow_error("Overflow after division.");
+                throw errOverflow;
             }
 
             auto quotient = static_cast<uint64_t>(temp);
@@ -163,7 +195,7 @@ class Decimal {
 
             return Decimal(quotient);
         } else {
-            double quotient = Double() / f0.Double();
+            double quotient = to_double() / f0.to_double();
             return Decimal(quotient);
         }
     }
@@ -175,15 +207,15 @@ class Decimal {
     bool operator>(const Decimal& rhs) const { return fp > rhs.fp; }
     bool operator>=(const Decimal& rhs) const { return fp >= rhs.fp; }
 
-    [[nodiscard]] std::string toString() const {
-        std::string s = toStr();
+    [[nodiscard]] std::string to_string() const {
+        std::string s = to_str();
 
-        int point = s.find('.');
+        std::size_t point = s.find('.');
         if (point == std::string::npos) {
             return s;
         }
 
-        int index = s.length() - 1;
+        std::size_t index = s.length() - 1;
         while (index != point) {
             if (s[index] != '0') {
                 return s.substr(0, index + 1);
@@ -194,10 +226,10 @@ class Decimal {
         return s.substr(0, point);
     }
 
-    [[nodiscard]] std::string toString(int decimals) const {
-        std::string s = toStr();
+    [[nodiscard]] std::string to_string(int decimals) const {
+        std::string s = to_str();
 
-        int point = s.find('.');
+        std::size_t point = s.find('.');
         if (point == std::string::npos) {
             return s;
         }
@@ -209,9 +241,9 @@ class Decimal {
         }
     }
 
-    [[nodiscard]] uint64_t toInt() const { return fp / scale; }
+    [[nodiscard]] uint64_t to_int() const { return fp / scale; }
 
-    [[nodiscard]] double toFrac() const { return static_cast<double>(fp % scale) / scale; }
+    [[nodiscard]] double to_frac() const { return static_cast<double>(fp % scale) / scale; }
 
     [[nodiscard]] Decimal round(int n) const {
         double round = 0.5;
@@ -219,19 +251,19 @@ class Decimal {
             round = -0.5;
         }
 
-        double f0 = toFrac();
-        f0 = f0 * pow(10, n) + round;
-        f0 = double(int(f0)) / pow(10, n);
+        double f0 = to_frac();
+        f0 = f0 * precomputed_pow_10(n) + round;
+        f0 = double(int(f0)) / precomputed_pow_10(n);
 
-        return {double(toInt()) + f0};
+        return {double(to_int()) + f0};
     }
 
-    // convert allows converting a Decimal from one precision to another.
+    // convert_precision allows converting a Decimal from one precision to another.
     // A conversion  moving the number of places right, will just be a lossy conversion
     // A conversion moving the number of places left will throw an overflow error
     // if it is not possible
     template <int toPlaces>
-    Decimal<toPlaces> convert() const {
+    Decimal<toPlaces> convert_precision() const {
         if constexpr (toPlaces == nPlaces) {
             return Decimal<toPlaces>(*this);
         } else if constexpr (toPlaces < nPlaces) {
@@ -240,14 +272,14 @@ class Decimal {
         } else {
             static constexpr uint64_t factor = const_pow<10, toPlaces>() / scale;
             if (unlikely(fp > std::numeric_limits<uint64_t>::max() / factor)) {
-                throw std::overflow_error("conversion overflow.");
+                throw errOverflow;
             }
             return Decimal<toPlaces>(fp * factor);
         }
     }
 
-    // DecodeBinary reads from a byte vector and sets the Decimal value
-    void DecodeBinary(const std::vector<uint8_t>& data) {
+    // decode_binary reads from a byte vector and sets the Decimal value
+    void decode_binary(const std::vector<uint8_t>& data) {
         uint64_t value = 0;
         int shift = 0;
         size_t index = 0;
@@ -272,9 +304,9 @@ class Decimal {
         fp = value;
     }
 
-    // DecodeBinaryData reads from a byte vector, sets the Decimal value, and returns the remaining data
-    std::vector<uint8_t> DecodeBinaryData(const std::vector<uint8_t>& data) {
-        DecodeBinary(data);
+    // decode_binary_data reads from a byte vector, sets the Decimal value, and returns the remaining data
+    std::vector<uint8_t> decode_binary_data(const std::vector<uint8_t>& data) {
+        decode_binary(data);
         size_t index = 0;
         for (; index < data.size() - 1; ++index) {
             if ((data[index] & 0x80) == 0) {
@@ -284,8 +316,8 @@ class Decimal {
         return {data.begin() + index + 2, data.end()};
     }
 
-    // EncodeBinary serializes the Decimal value into a byte vector
-    [[nodiscard]] std::vector<uint8_t> EncodeBinary() const {
+    // encode_binary serializes the Decimal value into a byte vector
+    [[nodiscard]] std::vector<uint8_t> encode_binary() const {
         std::vector<uint8_t> data;
         uint64_t value = fp;
         do {
@@ -316,7 +348,7 @@ class Decimal {
         if (fp0_a != 0) {
             result = fp_a * fp0_a;
             if (unlikely(static_cast<double>(result) > MAX)) {
-                throw std::overflow_error("decimal overflow");
+                throw errOverflow;
             }
             result = (result * scale) + (fp_b * fp0_a);
         }
@@ -330,16 +362,17 @@ class Decimal {
 
     static uint64_t newI(uint64_t i, uint32_t n) {
         if (n > nPlaces) {
-            i /= std::pow(10, n - nPlaces);
+            i /= precomputed_pow_10(n - nPlaces);
             n = nPlaces;
         }
-        i *= std::pow(10, nPlaces - n);
+        i *= precomputed_pow_10(nPlaces - n);
         return i;
     }
 
-    [[nodiscard]] std::string toStr() const {
+    [[nodiscard]] std::string to_str() const {
         if (fp == 0) {
-            return "0." + std::string(zeros());
+            static const std::string zero_str = "0." + zeros();
+            return zero_str;
         }
 
         std::array<char, 24> buf;
@@ -364,6 +397,13 @@ class Decimal {
     static int max(int a, int b) { return (a > b) ? a : b; }
 };
 
-static Decimal Zero = Decimal(uint64_t(0));
+template <int nPlaces>
+const std::runtime_error Decimal<nPlaces>::errDivByZero("division by zero");
+template <int nPlaces>
+const std::overflow_error Decimal<nPlaces>::errTooLarge("number is too large");
+template <int nPlaces>
+const std::overflow_error Decimal<nPlaces>::errOverflow("decimal overflow");
+template <int nPlaces>
+const std::invalid_argument Decimal<nPlaces>::errInvalidInput("invalid input");
 
 }  // namespace udecimal
